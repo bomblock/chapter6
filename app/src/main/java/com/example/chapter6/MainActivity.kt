9afe7f4dce47303a410736ed32d06879f077fb5b
bomblock <ekwotmd@gmail.com>
2023-05-27 12:11:42 +0900

    day2

--- app/src/main/java/com/example/chapter6/MainActivity.kt
+++ app/src/main/java/com/example/chapter6/MainActivity.kt
@@ -2,10 +2,45 @@ package com.example.chapter6
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.chapter6.databinding.ActivityMainBinding
+import kotlinx.coroutines.NonCancellable.start
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startButton.setOnClickListener{
+            start()
+        }
+        binding.stoptButton.setOnClickListener {
+            stop()
+        }
+        binding.pauseButton.setOnClickListener {
+            pause()
+        }
+        binding.lapButton.setOnClickListener {
+            lap()
+        }
+
+
     }
+
+    private fun start(){
+
+    }
+
+    private fun pause(){
+
+    }
+
+    private  fun  stop(){
+
+    }
+    private  fun lap(){
+
+    }
+
 }
