8154b82c4ef5cbb57472bcf73979d60788f304e3
bomblock <ekwotmd@gmail.com>
2023-05-29 23:34:56 +0900

    day3 last

--- app/src/main/java/com/example/chapter6/MainActivity.kt
+++ app/src/main/java/com/example/chapter6/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.chapter6
 
+import android.media.AudioManager
+import android.media.ToneGenerator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.core.view.isVisible
+import androidx.core.view.setPadding
 import com.example.chapter6.databinding.ActivityMainBinding
 import com.example.chapter6.databinding.DialogCountdownSettingBinding
 import kotlinx.coroutines.NonCancellable.start
@@ -11,22 +16,22 @@ import java.util.Timer
 import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
-    private var countdownSecond = 10
+    private lateinit var binding: ActivityMainBinding
+    private var countdownSecond = 5
     private var currentCountdownDeciSecond = countdownSecond * 10
     private var currentDeciSecond = 0
-    private  var timer : Timer? = null
+    private var timer: Timer? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.countdownTextView.setOnClickListener{
+        binding.countdownTextView.setOnClickListener {
             showCountdownSettingDialog()
         }
 
 
-        binding.startButton.setOnClickListener{
+        binding.startButton.setOnClickListener {
             start()
             binding.startButton.isVisible = false
             binding.stopButton.isVisible = false
@@ -35,7 +40,6 @@ class MainActivity : AppCompatActivity() {
         }
         binding.stopButton.setOnClickListener {
             showAlertDialog()
-
         }
         binding.pauseButton.setOnClickListener {
             pause()
@@ -48,85 +52,123 @@ class MainActivity : AppCompatActivity() {
             lap()
 
         }
+        initCountdownViews()
     }
 
-    private fun start(){
+    private fun initCountdownViews() {
+        val seconds = currentCountdownDeciSecond / 10
+        binding.countdownTextView.text = String.format("%02d", countdownSecond)
+
+        binding.countdownProgressBar.progress = 100
+    }
+
+    private fun start() {
         timer = timer(initialDelay = 0, period = 100) {
-            if(currentCountdownDeciSecond == 0) {
+            if (currentCountdownDeciSecond == 0) {
                 currentDeciSecond += 1
 
-                val minutes = currentDeciSecond.div(10)/60
-                val seconds = currentDeciSecond.div(10)%60
-                val deciSeconds = currentDeciSecond%10
+                val minutes = currentDeciSecond.div(10) / 60
+                val seconds = currentDeciSecond.div(10) % 60
+                val deciSeconds = currentDeciSecond % 10
 
                 runOnUiThread {
                     binding.timeTextview.text =
                         String.format("%02d:%02d", minutes, seconds)
                     binding.tickTextView.text = deciSeconds.toString()
+
+                    binding.countdownGroup.isVisible = false
                 }
             } else {
                 currentCountdownDeciSecond -= 1
-                val seconds = currentCountdownDeciSecond /10
+                val seconds = currentCountdownDeciSecond / 10
                 val progress = (currentCountdownDeciSecond / (countdownSecond * 10f)) * 100
 
-                binding.root.post{
+                binding.root.post {
                     binding.countdownTextView.text = String.format("%02d", seconds)
                     binding.countdownProgressBar.progress = progress.toInt()
                 }
             }
+            if  (currentDeciSecond == 0 && currentCountdownDeciSecond < 31 && currentCountdownDeciSecond % 10 == 0) {
+                val toneType =
+                    if (currentCountdownDeciSecond == 0) ToneGenerator.TONE_CDMA_HIGH_L else ToneGenerator.TONE_CDMA_ANSWER
+                ToneGenerator(AudioManager.STREAM_ALARM, ToneGenerator.MAX_VOLUME)
+                    .startTone(toneType, 100)
+            }
         }
     }
 
-    private fun pause(){
+    private fun pause() {
         timer?.cancel()
         timer = null
 
     }
 
-    private  fun  stop(){
+    private fun stop() {
         binding.startButton.isVisible = true
         binding.stopButton.isVisible = true
         binding.pauseButton.isVisible = false
         binding.lapButton.isVisible = false
 
         currentDeciSecond = 0
-        binding.timeTextview.text ="00:00"
+        binding.timeTextview.text = "00:00"
         binding.tickTextView.text = "0"
 
+        binding.countdownGroup.isVisible = true
+        initCountdownViews()
+        binding.lapContainerLinearLayout.removeAllViews()
     }
-    private  fun lap(){
 
+    private fun lap() {
+        if (currentDeciSecond == 0) return
+        val container = binding.lapContainerLinearLayout
+        TextView(this).apply {
+            textSize = 20f
+            gravity = Gravity.CENTER
+            val minutes = currentDeciSecond.div(10) / 60
+            val seconds = currentDeciSecond.div(10) % 60
+            val deciSeconds = currentDeciSecond % 10
+            text = " ${container.childCount.inc()}. " + String.format(
+                "%02d:%02d %01d",
+                minutes,
+                seconds,
+                deciSeconds
+
+            )
+            setPadding(30)
+        }.let { labTextView ->
+            container.addView(labTextView, 0)
+        }
     }
 
     private fun showCountdownSettingDialog() {
         AlertDialog.Builder(this).apply {
             val dialogBinding = DialogCountdownSettingBinding.inflate(layoutInflater)
             with(dialogBinding.countdownSecondPicker) {
-                maxValue =20
+                maxValue = 20
                 minValue = 0
-                value =countdownSecond
+                value = countdownSecond
             }
             setTitle("카운트다운 설정")
             setView(dialogBinding.root)
             setPositiveButton("확인") { _, _ ->
-                countdownSecond =dialogBinding.countdownSecondPicker.value
+                countdownSecond = dialogBinding.countdownSecondPicker.value
                 currentCountdownDeciSecond = countdownSecond * 10
                 binding.countdownTextView.text = String.format("%02d", countdownSecond)
             }
-            setNegativeButton("취소", null )
+            setNegativeButton("취소", null)
         }.show()
     }
 
     private fun showAlertDialog() {
         AlertDialog.Builder(this).apply {
             setMessage("종료하시겠습니까?")
-            setPositiveButton("네",){ _, _ ->
+            setPositiveButton("네") { _, _ ->
                 stop()
 
             }
             setNegativeButton("아니요", null)
 
-        }
+        }.show()
     }
 
 }
